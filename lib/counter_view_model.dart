81690ebaa2ab33caad0b4ed20cb74bf78652228a
Junsuk Oh <a811219@gmail.com>
2021-06-22 16:54:58 +0900

    MVVM 적용

--- lib/counter_view_model.dart
+++ lib/counter_view_model.dart
@@ -1,11 +1,15 @@
 import 'package:counter/repository/repository.dart';
+import 'package:flutter/foundation.dart';
 
-class CounterViewModel {
+class CounterViewModel extends ChangeNotifier {
   Repository _repository;
 
   CounterViewModel(this._repository);
 
   int get count => _repository.value;
 
-  void increment() => _repository.add();
+  void increment() {
+    _repository.add();
+    notifyListeners();
+  }
 }
